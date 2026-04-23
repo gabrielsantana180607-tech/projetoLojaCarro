ad6780862c81c07b5d8d211907d8ec9a309e55ab
gabrielsantana180607-tech <gabrielsantana180607@gmail.com>
2026-04-23 17:28:30 -0300

    Update script.js

---
+++ script/script.js
@@ -0,0 +1,34 @@
+function salvarCarro (event){
+    //impedir carregar a pagina
+    event.preventDefault();
+    let.titulo = document.getElementById ('tittle')
+    let.preco = document.getElementById ('preco')
+    let.marca = document.getElementById ('marca')
+    let.modelo = document.getElementById ('Modelo')
+
+    cambioSelecionado = document.querySelector ("input [name='marcha']:checked")
+    let cambio= cambioSelecionado ? cambioSelecionado.id: 'Não informado';
+    let lista= document.getElementById('listarCarro');
+    let card= document.createElement('div');
+    
+    card.style.border = '1px solid #ccc';
+    card.style.padding = '10px';
+    card.style.marginBottom ='10px'
+
+    card.innerHTML= '<h3>${titulo}</h3>
+    <p> <strong> Preco:</strong> R${preco} </p>
+    <p> <strong> Marca:</strong> R${marca}</p>
+    <p> <strong> Modelo:</strong> R${modelo}</p>
+    <p> <strong> Câmbio:</strong> R${cambio}</p>'
+         lista.appendChild(card);'
+          let carro = {
+            titulo,preco,marca,modelo,cambio
+          };
+          let carros= JSON.parse(localStorage.getItem('carros'))||[]
+          carros.push(carro);
+          localStorage.setItem('carros', JSON.stringify(carros)')
+
+
+
+}
+ 
